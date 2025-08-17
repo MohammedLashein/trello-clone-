be0ada2c00a758aaaf24850702be8b37afbdfcd2
Mohammed Lashein <ben10gravattack@gmail.com>
2025-08-17 11:20:06 +0000

    feat: add signup functionality to the navbar

--- my-app/src/app/middleware.ts
+++
@@ -1,6 +0,0 @@
-export function middleware() {
-    console.log("hello from the middleware")
-}
-export const config = {
-    matcher: "/"
-}
--- my-app/src/components/navbar.tsx
+++ my-app/src/components/navbar.tsx
@@ -1,15 +1,37 @@
+'use client'
 import { Trello } from "lucide-react";
-
+import { Button } from "./ui/button";
+import { SignInButton, SignUpButton, useUser } from "@clerk/nextjs";
 export function Navbar() {
+    console.log(useUser())
+    console.log(useUser().user?.emailAddresses)
+    console.log(useUser().user?.emailAddresses[0].emailAddress)
+    let isUserSignedIn = useUser().isSignedIn
+    let doesUsernameExist = true
     // useUser hook from clerk returns all of the info \
     // we need about a certain user
-    return <div>
-        hello navbar
-        <div>
+    return <div className="flex items-center justify-between p-2 sm: p-4 backdrop-blur-sm border">
+        <div className="flex space-x-2">
             <Trello />
             <span>
                 trello clone
             </span>
         </div>
+
+        {isUserSignedIn 
+        ? (
+            <div>Welcome, {useUser()?.user?.username ?? useUser().user?.emailAddresses[0].emailAddress}</div>
+        )
+        : (
+            <div className="text-xs sm:text-sm flex gap-1">
+                <SignInButton>
+
+                <Button variant='ghost'>Login </Button>
+                </SignInButton>
+                <SignUpButton>
+                <Button>Sign up </Button>
+                </SignUpButton>
+            </div>
+        )}
     </div>
 }
